a75e678148d20e89263bf2f98e517bab0382af56
naufalammarr <135613598+naufalammarr@users.noreply.github.com>
2024-05-06 02:35:02 +0700

    menambahkan dan implementasi aljabar libraries

---
+++ AljabarImplementation/AljabarImplementation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AljabarLibraries;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] akar = Aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+            Console.WriteLine("Akar Persamaan Kuadrat: {0}", FormatArray(akar));
+
+            double[] hasilKuadrat = Aljabar.HasilKuadrat(new double[] { 2, -3 });
+            Console.WriteLine("Hasil Kuadrat: {0}", FormatArray(hasilKuadrat));
+        }
+
+        static string FormatArray(double[] array)
+        {
+            return "{" + string.Join(", ", array) + "}";
+        }
+    }
+}
